a0685bf7c96f03963b1b1093d0957a2eb6c6b075
Dan Tche <dtcheche@gmail.com>
2023-06-08 15:30:31 -0300

    Setup env variables and settings

---
+++ src/config/config.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME = "Auth Service"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: str
+    DATABASE_HOSTNAME: str
+    ENVIRONMENT: str
+
+    REFRESH_TOKEN_EXPIRES_IN: int
+    ACCESS_TOKEN_EXPIRES_IN: int
+    JWT_ALGORITHM: str
+
+    class Config:
+        env_file = "./.env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
+APP_CONFIGS = {"title": settings.APP_NAME}
+if settings.ENVIRONMENT != "local":
+    APP_CONFIGS["openapi_url"] = None
+
+
+@lru_cache()
+def get_settings():
+    return settings
+
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+from .config.config import APP_CONFIGS, get_settings
+
+app = FastAPI(**APP_CONFIGS)
+
+settings = get_settings()
