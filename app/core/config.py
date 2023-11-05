64a779314368a73a710d975831bb7c2c4f0d22e7
Dan Tche <dtcheche@gmail.com>
2023-11-05 15:06:05 -0300

    Add config to settings

--- app/core/config.py
+++ app/core/config.py
@@ -7,5 +7,10 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_PORT: str = "5432"
 
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 settings = Settings()
