d55b16daed8eb94f18717c552b27baf32fbb0bac
Dan Tche <dtcheche@gmail.com>
2024-02-19 23:14:12 -0300

    Linter

--- app/core/security.py
+++ app/core/security.py
@@ -12,15 +12,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
 
 
-def create_access_token(
-    subject: Union[str, Any], expires_delta_minutes: int = 0
-) -> str:
-    if expires_delta_minutes:
-        expire = datetime.utcnow() + timedelta(minutes=expires_delta_minutes)
-    else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
+def create_access_token(subject: Union[str, Any], expires_delta_minutes: int) -> str:
+    expire = datetime.utcnow() + timedelta(minutes=expires_delta_minutes)
     to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
