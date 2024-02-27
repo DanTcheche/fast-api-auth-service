01ae3eaa0dc3a0f8e5b716ab0fbde6789787c308
Dan Tche <dtcheche@gmail.com>
2024-02-27 16:33:18 -0300

    Replace sqlmodel with sql alchemy

--- app/models/base_class.py
+++ app/models/base_class.py
@@ -1,8 +1,29 @@
+import uuid
 from datetime import datetime
 
-from sqlmodel import Field, SQLModel
+from sqlalchemy import UUID, Column, DateTime, func
+from sqlalchemy.orm import Mapped, declared_attr, registry
+
+mapper_registry: registry = registry()
 
 
-class Base(SQLModel):
-    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
-    updated_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+@mapper_registry.as_declarative_base()
+class Base:
+    __abstract__ = True
+
+    id: Mapped[uuid.UUID] = Column(
+        UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, nullable=False
+    )
+    time_created: Mapped[datetime] = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    time_updated: Mapped[datetime] = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
