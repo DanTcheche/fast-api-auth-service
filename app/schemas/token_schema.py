38059e1a800eb6570126c71f5b6cd356f4b5cba1
Dan Tche <dtcheche@gmail.com>
2024-03-15 15:41:21 -0300

    Rename schemas

--- app/schemas/token_schema.py
+++ app/schemas/token_schema.py
@@ -1,12 +1,10 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
 
 
+# Contents of JWT token
 class TokenPayload(BaseModel):
-    sub: Optional[int] = None
+    user_id: str | None = None
